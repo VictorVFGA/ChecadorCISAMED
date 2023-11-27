b12a34dd51dfe27b54bb9a071d859d416db4a08e
VictorVFGA <131395417+VictorVFGA@users.noreply.github.com>
2023-11-27 10:32:27 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,29 +2,41 @@
 if (isset($_GET['data'])) {
     $data = $_GET['data'];
 
-    $conn = new mysqli("db", "victor", "pass123", "checador");
+    $conn = new mysqli("localhost", "root", "", "checador");
 
     if ($conn->connect_error) {
         die("Error en la conexión: " . $conn->connect_error);
     }
-    
-    $sql = "INSERT INTO regis_ingreso (id_emple) VALUES ('$data')";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Los datos fueron insertados en la tabla regis_ingreso de manera correcta";
-    } else {
-        //echo "Error al insertar los datos en la tabla regis_ingreso: " . $conn->error;s
-        $sql_asistencia = "INSERT INTO asistencia_alum (IDAlum) VALUES ('$data')";
-        if ($conn->query($sql_asistencia) === TRUE) {
-            //echo "Los datos se insertaron en la tabla asistencia_alum de manera correcta";
+
+    // Verifica si el valor ya existe en la tabla 
+    $check_existing_query2 = "SELECT id_emple FROM datos_emple WHERE id_emple = '$data'";
+    $result_existing2 = $conn->query($check_existing_query2);
+    $check_existing_query = "SELECT IDAlumn FROM datos_alumno WHERE IDAlumn = '$data'";
+    $result_existing = $conn->query($check_existing_query);
+
+    if ($result_existing2->num_rows > 0) {
+        $sql_regis_ingreso = "INSERT INTO regis_ingreso (id_emple) VALUES ('$data');";
+        echo "Valor de data: " . $data . PHP_EOL;
+        if ($conn->query($sql_regis_ingreso) === TRUE) {
+            echo "Inserte correctamente";
         } else {
-            echo "Error al insertar los datos en la tabla asistencia_alum: " . $conn->error;
+                echo "Error al insertar: " . $conn->error;
+        }
+    
+    } else if($result_existing->num_rows > 0){
+        $sql_asistencia = "INSERT INTO asistencia_alum (IDAlumn) VALUES ('$data')";
+        echo "Valor de data: " . $data . PHP_EOL;
+        if($conn->query($sql_asistencia) === TRUE){
+            echo "Inserte correctamente";
+        }else{
+            echo "Error al insertar: " . $conn->error;
         }
     }
-
+    else {
+        echo "Error al insertar los datos en ambas tablas: " . $conn->error;
+    }
     $conn->close();
 } else {
-    echo "No Recibí data en la URL";
+    echo "No recibí data en la solicitud.";
 }
-
 ?>
